b1e2807b5d96773931be7711e6310b8b8e8f72d9
Paradox-3arthling <floydqaranja@gmail.com>
2020-07-06 15:40:11 +0300

    Formatting of response

--- authentication.go
+++ authentication.go
@@ -1,41 +1,56 @@
 package daraja_wrapper
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type Token struct {
+	Token  string `json:"access_token"`
+	Expiry string `json:"expires_in"`
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("Token: '%s', Expires in(seconds): '%s'", t.Token, t.Expiry)
+}
 
 type Auth struct {
 	Key, Secret string
 	Prod        bool
 }
 
-func (a *Auth) GetAuthKey() (map[string]string, error) {
+func (a Auth) String() string {
+	return fmt.Sprintf("Key: '%s', Secret: '%s', Prod: '%v'", a.Key, a.Secret, a.Prod)
+}
+func (a *Auth) GetAuthKey() (Token, error) {
 	client := &http.Client{}
 	url := a.setUrl()
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return Token{}, err
 	}
 	req.SetBasicAuth(a.Key, a.Secret)
 	req.Header.Add("Accept", "application/json")
 	if err != nil {
-		return nil, err
+		return Token{}, err
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return Token{}, err
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return Token{}, err
 	}
-	fmt.Println(string(body))
-	// Do safaricom request here
-	// convert response to a map from json
-	return make(map[string]string), errors.New("Boiler plate code inserted, will fail")
+	var token Token
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		return token, err
+	}
+	fmt.Println(token)
+	return token, nil
 }
 func (a *Auth) setUrl() string {
 	url := "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
